74798d5aed80989b11305fe78681e1b3f0fb2a8d
夏湾 <2693066151@qq.com>
2022-02-22 09:30:19 +0800

    Update

--- SR_DebugHelperExtension/modmain.lua
+++ SR_DebugHelperExtension/modmain.lua
@@ -1,159 +1,82 @@
 GLOBAL.setmetatable(env,{__index=function(t,k) return GLOBAL.rawget(t,k) or GLOBAL.rawget(GLOBAL,k) end})
 
-function SendClientRPC(name,...)
-    SendModRPCToClient(CLIENT_MOD_RPC[modname][name], ThePlayer.userid,...)
-end
+modimport("scripts/helpers")  
 
---------------------------------Different
-function AddServerCommand(fnname,fn)
-    if fn and not GLOBAL.rawget(GLOBAL, fnname) then 
-        GLOBAL.global(fnname)  --注册到global
-        GLOBAL.rawset(GLOBAL, fnname, fn)
-    else
-        moderror("Server: This fnname is existed. ".. fnname)
-    end
-end
-
-AddClientModRPCHandler(modname, "printf", function(str) print(str) end)
-AddServerCommand("printf", function(...)
+local function GetTableStr(tb)
+    -- key=value:type,
     local str = ""
-    for _, v in pairs({...}) do
-        str = str .. tostring(v) .. "\t"
+    local vtype = nil
+    local vstr = ""
+    for k, v in pairs(tb) do
+        modassert(type(k)=="string" or type(k)=="number","The key of table must be string or number!")
+        vtype = type(v)
+        if vtype=="table" then
+            vstr = " :table"
+        elseif vtype=="function" then
+            vstr = " :function"
+        elseif vtype=="userdata" then
+            vstr = " :userdata"
+        else
+            vstr = tostring(v)..":"..vtype
+        end
+        str = str..tostring(k).."="..vstr..","
     end
-    if str=="" then str = "nil" end
-    SendModRPCToClient(CLIENT_MOD_RPC[modname]["printf"], ThePlayer.userid, str)
-end)
 
-AddServerCommand("sr_print", function(...)
-    local str = ""
-    for _,v in pairs({...}) do
-        str = str.."sr_print: "..tostring(v).."\n"
-        if type(v)=="table" then
-            for key, value in pairs(v) do
-                str = str.."\t"..tostring(key).." "..tostring(value).."\n"
+    local meta = getmetatable(tb)
+    if meta then
+        for k, v in pairs(meta) do
+            modassert(type(k)=="string" or type(k)=="number","The key of table must be string or number!")
+            vtype = type(v)
+            if vtype=="table" then
+                vstr = " :table"
+            elseif vtype=="function" then
+                vstr = " :function"
+            elseif vtype=="userdata" then
+                vstr = " :userdata"
+            else
+                vstr = tostring(v)..":"..vtype
             end
+            str = str..tostring(k).."="..vstr..","
         end
     end
-    if str=="" then str = "nil" end
-    printf(str)
-end)
+    return str
+end
 
-AddServerCommand("sr_say", function(str)
-    if ThePlayer and ThePlayer.components.talker then 
-        ThePlayer.components.talker:Say(tostring(str))
-    else
-        printf(str) 
-    end 
-end)
-
-local lastinst = nil
-AddServerCommand("sr_findinst", function (x,y,z)
-    if x and y and z then
-        local entitys = TheSim:FindEntities(x,y,z,1,{},{})
-        lastinst = entitys and entitys[1] or nil
-    else
-        lastinst = nil
-    end
-    return lastinst
-end)
-AddClientModRPCHandler(modname, "sr_findinst", function() 
-    local inst = s_get()
-    if inst then
-        local x,y,z = inst.Transform:GetWorldPosition()
-        c_remote("sr_findinst("..x..","..y..","..z..")")
-    else
-        c_remote("sr_findinst()")
-    end
-end)
-AddServerCommand("sr_get", function (guid)
-    SendClientRPC("sr_findinst")
-end)
-
-AddServerCommand("sr_inst", function()
-    return lastinst
-end)
-
-AddServerCommand("sr_show", function(tb)
+-------------------------------For sr_show
+AddModRPCHandler(modname, "Menu_Clear", function(player) 
 
 end)
 
-AddServerCommand("sr_data", function()
+AddModRPCHandler(modname, "Menu_Pop", function(player) 
 
 end)
 
-AddClientModRPCHandler(modname, "sr_help", function() s_help() end)
-AddServerCommand("sr_help", function ()
-    SendClientRPC("sr_help")
-    local help = "sr_xxx is similar to s_xxx.\n"..
-        "Different:\n\tsr_get() will no return value;\n\tsr_inst() only return lastvalue.\n"..
-        "Add:\n"..
-        "\tsr_findinst(x,y,z): get the inst in world position.\n"..
-        "\tprintf(...): print something to client, you can also use sr_print(...)."
+-- local tbstack = {}
+-- AddModRPCHandler("sr_debughelperextension", "changevalue", function (player,key,value)
+--     if #tbstack==0 then return end
+--     local tb = tbstack[#tbstack]
+--     tb[key] = value
+-- end)
+-- AddModRPCHandler("sr_debughelperextension", "push", function (player,key)
+--     if #tbstack==0 then return end
+--     local tb = tbstack[#tbstack]
+--     local vtype = type(tb[key])
+--     if vtype=="table" then
 
-    printf(help)
-end)
+--     elseif vtype=="function" then
 
--- same as s_xxx 
+--     elseif vtype=="userdata" then
 
-AddServerCommand("sr_component", function (comp)
-    for name,value in pairs(comp) do
-		if type(value) == "function" then
-			local info = debug.getinfo(value,"LnS")
-			printf(string.format("      %s = function - %s", name, info.source..":"..tostring(info.linedefined)))
-		else
-			if value and type(value) == "table" and value.IsValid and type(value.IsValid) == "function" then
-                printf(string.format("      %s = %s (valid:%s)", name, tostring(value),tostring(value:IsValid())))
-			else
-                printf(string.format("      %s = %s", name, tostring(value)))
-			end
-		end
-	end
-end)
-
-AddServerCommand("sr_entity",function (ent)
-	printf("============================================ Dumping entity ",ent,"============================================")
-	printf(ent.entity:GetDebugString())
-	printf("--------------------------------------------------------------------------------------------------------------------")
-	for name,value in pairs(ent) do
-		if type(value) == "function" then
-			local info = debug.getinfo(value,"LnS")
-			printf(string.format("   %s = function - %s", name, info.source..":"..tostring(info.linedefined)))
-		else
-			if value and type(value) == "table" and value.IsValid and type(value.IsValid) == "function" then
-			   printf(string.format("   %s = %s (valid:%s)", name, tostring(value),tostring(value:IsValid())))
-			else
-			   printf(string.format("   %s = %s", name, tostring(value)))
-			end
-		end
-	end
-	printf("--------------------------------------------------------------------------------------------------------------------")
-	for i,v in pairs(ent.components) do
-		printf("   Dumping component",i)
-		DumpComponent(v)
-	end
-	printf("====================================================================================================================================")
-end)
-
-AddServerCommand("sr_upvalues",function (func)
-	printf("============================================ Dumping Upvalues ============================================")
-	local info = debug.getinfo(func, "nuS")
-	printf(string.format("Upvalues (%d of 'em) for function %s defined at %s:%s", info.nups, tostring(info.name), tostring(info.source), tostring(info.linedefined)))
-    for i = 1,math.huge do
-        local name, v = debug.getupvalue(func,i)
-        if name == nil then break end
-        printf(string.format("%d:\t %s (%s) \t= %s", i,name,type(v),tostring(v)))
-    end
-	printf("--------------------------------------------------------------------------------------------------------------------")
-end)
-
-AddServerCommand("sr_fn", function(fn)
-    local tb = debug.getinfo(fn,"S")
-    if tb==nil then printf("sr_fn fails to get the info of fn") return end
-    printf(fn,tb.what,tb.source)
-end)
-
-
---Debug
-AddServerCommand("sr_test", function()
-
-end)
+--     end
+-- end)
+-- AddClientModRPCHandler("sr_debughelperextension", "push",function (str)
+--     local SR_DEBUGMENU = ThePlayer and ThePlayer.SR_DEBUGMENU
+--     if SR_DEBUGMENU then
+        
+--     end
+-- end)
+-- AddModRPCHandler("sr_debughelperextension", "pop", function ()
+--     if #tbstack==0 then return end
+--     local tb = tbstack[#tbstack]
+--     tb[key] = value
+-- end)
---
+++ SR_DebugHelperExtension/scripts/helpers.lua
@@ -0,0 +1,173 @@
+function SendClientRPC(name,...)
+    SendModRPCToClient(CLIENT_MOD_RPC[modname][name], ThePlayer.userid,...)
+end
+
+--------------------------------Different
+function AddServerCommand(fnname,fn)
+    if fn and not GLOBAL.rawget(GLOBAL, fnname) then 
+        GLOBAL.global(fnname)  --注册到global
+        GLOBAL.rawset(GLOBAL, fnname, fn)
+    else
+        moderror("Server: This fnname is existed. ".. fnname)
+    end
+end
+
+AddClientModRPCHandler(modname, "printf", function(str) print(str) end)
+AddServerCommand("printf", function(...)
+    if TheNet:GetIsClient() then print(...) return end
+
+    local str = ""
+    for _, v in pairs({...}) do
+        str = str .. tostring(v) .. "\t"
+    end
+    if str=="" then str = "nil" end
+    SendModRPCToClient(CLIENT_MOD_RPC[modname]["printf"], ThePlayer.userid, str)
+end)
+
+AddServerCommand("sr_print", function(...)
+    local str = ""
+    for _,v in pairs({...}) do
+        str = str.."sr_print: "..tostring(v).."\n"
+        if type(v)=="table" then
+            for key, value in pairs(v) do
+                str = str.."\t"..tostring(key).." "..tostring(value).."\n"
+            end
+        end
+    end
+    if str=="" then str = "nil" end
+    printf(str)
+end)
+
+AddServerCommand("sr_say", function(str)
+    if ThePlayer and ThePlayer.components.talker then 
+        ThePlayer.components.talker:Say(tostring(str))
+    else
+        printf(str) 
+    end 
+end)
+
+local lastinst = nil
+AddServerCommand("sr_findinst", function (x,y,z)
+    if x and y and z then
+        local entitys = TheSim:FindEntities(x,y,z,1,{},{})
+        lastinst = entitys and entitys[1] or nil
+    else
+        lastinst = nil
+    end
+    return lastinst
+end)
+AddClientModRPCHandler(modname, "sr_findinst", function() 
+    local inst = s_get()
+    if inst then
+        local x,y,z = inst.Transform:GetWorldPosition()
+        c_remote("sr_findinst("..x..","..y..","..z..")")
+    else
+        c_remote("sr_findinst()")
+    end
+end)
+AddServerCommand("sr_get", function ()
+    if TheNet:GetIsClient() then s_get() return end
+    SendClientRPC("sr_findinst")
+end)
+
+AddServerCommand("sr_inst", function()
+    return TheNet:GetIsClient() and s_inst() or lastinst
+end)
+
+AddServerCommand("sr_show", function(tb)
+    if TheNet:GetIsClient() then s_show(tb) return end 
+end)
+
+AddServerCommand("sr_data", function()
+    if TheNet:GetIsClient() then return s_data() end 
+end)
+
+AddClientModRPCHandler(modname, "sr_help", function() s_help() end)
+AddServerCommand("sr_help", function ()
+    if TheNet:GetIsClient() then 
+        s_help()
+    else 
+        SendClientRPC("sr_help")
+    end
+    local help = "sr_xxx is similar to s_xxx.\n"..
+        "Different:\n\tsr_get() will no return value;\n\tsr_inst() only return lastvalue.\n"..
+        "Add:\n"..
+        "\tsr_findinst(x,y,z): get the inst in world position.\n"..
+        "\tprintf(...): print something to client, you can also use sr_print(...)."
+
+    printf(help)
+end)
+
+-- same as s_xxx 
+
+AddServerCommand("sr_component", function (comp)
+    for name,value in pairs(comp) do
+		if type(value) == "function" then
+			local info = debug.getinfo(value,"LnS")
+			printf(string.format("      %s = function - %s", name, info.source..":"..tostring(info.linedefined)))
+		else
+			if value and type(value) == "table" and value.IsValid and type(value.IsValid) == "function" then
+                printf(string.format("      %s = %s (valid:%s)", name, tostring(value),tostring(value:IsValid())))
+			else
+                printf(string.format("      %s = %s", name, tostring(value)))
+			end
+		end
+	end
+end)
+
+AddServerCommand("sr_entity",function (ent)
+	printf("============================================ Dumping entity ",ent,"============================================")
+	printf(ent.entity:GetDebugString())
+	printf("--------------------------------------------------------------------------------------------------------------------")
+	for name,value in pairs(ent) do
+		if type(value) == "function" then
+			local info = debug.getinfo(value,"LnS")
+			printf(string.format("   %s = function - %s", name, info.source..":"..tostring(info.linedefined)))
+		else
+			if value and type(value) == "table" and value.IsValid and type(value.IsValid) == "function" then
+			   printf(string.format("   %s = %s (valid:%s)", name, tostring(value),tostring(value:IsValid())))
+			else
+			   printf(string.format("   %s = %s", name, tostring(value)))
+			end
+		end
+	end
+	printf("--------------------------------------------------------------------------------------------------------------------")
+	for i,v in pairs(ent.components) do
+		printf("   Dumping component",i)
+		DumpComponent(v)
+	end
+	printf("====================================================================================================================================")
+end)
+
+AddServerCommand("sr_upvalues",function (func)
+	printf("============================================ Dumping Upvalues ============================================")
+	local info = debug.getinfo(func, "nuS")
+	printf(string.format("Upvalues (%d of 'em) for function %s defined at %s:%s", info.nups, tostring(info.name), tostring(info.source), tostring(info.linedefined)))
+    for i = 1,math.huge do
+        local name, v = debug.getupvalue(func,i)
+        if name == nil then break end
+        printf(string.format("%d:\t %s (%s) \t= %s", i,name,type(v),tostring(v)))
+    end
+	printf("--------------------------------------------------------------------------------------------------------------------")
+end)
+
+AddServerCommand("sr_fn", function(fn)
+    local tb = debug.getinfo(fn,"S")
+    if tb==nil then printf("sr_fn fails to get the info of fn") return end
+    printf(fn,tb.what,tb.source)
+end)
+
+
+--Debug
+AddServerCommand("sr_test", function()
+    ThePlayer.str1 = net_string(ThePlayer.GUID,"str1","str1")
+    ThePlayer.str2 = net_string(ThePlayer.GUID,"str2","str2")
+    if TheNet:GetIsClient() then
+        ThePlayer:ListenForEvent("str1",function (...)
+            printf(...)
+        end)
+        ThePlayer:ListenForEvent("str2",function (...)
+            printf(...)
+        end)
+    end
+end)
